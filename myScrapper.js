30ee24c4a3285ee76962a9f76053eff395863b7a
Abhishek-2k19 <abhiamisagar2512@gmail.com>
2023-06-08 18:11:11 +0530

    Restructured code, used promise framework

--- myScrapper.js
+++ myScrapper.js
@@ -1,70 +1,41 @@
 const puppeteer = require('puppeteer');
 const { JSDOM } = require('jsdom');
 const fs = require("fs");
+const { resolve } = require('path');
 
 const homepageLink = 'https://leetcode.com/problemset/all/';
 
-async function scrapeLeetCodeQuestions() {
-  const browser = await puppeteer.launch('headless:false');
+async function scrapeLeetCodeQuestions(pageNumber,browser) {
+  const pageLink = homepageLink + `?page=${pageNumber}`;
+  // await page.goto(pageLink);  
+  console.log(`Scrapping from : ${pageLink}`);
   const page = await browser.newPage();
-  await page.goto(homepageLink);
+  await page.goto(pageLink);
 
-  // await page.waitForSelector('.h-5 hover:text-blue-s dark:hover:text-dark-blue-s',{ timeout: 120000 });
-
-  // waiting for page to load the dynamic part
   await page.waitForFunction(() => {
     // Check if the desired element is present or any specific condition is met
     return document.querySelector('.h-5.hover\\:text-blue-s.dark\\:hover\\:text-dark-blue-s') !== null;
   });
-
 
   // taking page data for dom manipulation
   const pageData = await page.content()
   const dom = new JSDOM(pageData);
   const document = dom.window.document;
 
+  const anchorTags = document.getElementsByTagName('a');
 
-  // finding the total number of pages
-
-  const container = document.getElementsByClassName("mb-6 md:mb-0 flex flex-nowrap items-center space-x-2");
-  // console.log(container);
-  // const array = Array.from(container);
-  // console.log(array[0].innerHTML);
-  
-  const buttons = container[0].querySelectorAll("button");
-  // console.log(buttons.length);
-  let numPages = 1;
-  buttons.forEach((button) => {
-    // Perform actions on each button
-    // console.log(button.textContent); // Example action: Print button text
-    // console.log(typeof button.textContent); // Example action: Print button text
-    let newStr = button.textContent.trim();
-    if(newStr!=""){
-      const pgN = parseInt(newStr);
-      if(!isNaN(pgN)){
-        numPages = Math.max(numPages,pgN);
-      }
-    }
-  });
-
-  console.log(numPages);
-
-
-  for(let i=1;i<=numPages;i++){
-    const pageLink = homepageLink + `?page=${i}`;
-    await page.goto(pageLink);
+  const linksArray = [];
+  for (let i = 0; i < anchorTags.length; i++) {
+    // console.log(anchorTags[i].getAttribute('href'));
+    linksArray.push(anchorTags[i].getAttribute('href'));
   }
 
-  // const anchorTags = document.getElementsByTagName('a');
+  console.log(linksArray.length);
+  fs.writeFileSync(`links${pageNumber}.txt`, linksArray.join('\n'));
 
-  // const linksArray = [];
-  // for (let i = 0; i < anchorTags.length; i++) {
-  //   // console.log(anchorTags[i].getAttribute('href'));
-  //   linksArray.push(anchorTags[i].getAttribute('href'));
-  // }
-
-  // console.log(linksArray.length);
-  // fs.writeFileSync("links.txt", linksArray.join('\n'));
+  await page.close();
+}
+  
 
   // const html = await page.content();
   // fs.writeFileSync("index.html", html);
@@ -72,11 +43,56 @@ async function scrapeLeetCodeQuestions() {
   // Wait for the page to load the questions
   // await page.waitFor(".mb-6 md:mb-0 flex flex-nowrap items-center space-x-2");
 
-  
 
 
+function getNumberOfPages(){
+  return new Promise(async(resolve,reject)=> {
+    const browser = await puppeteer.launch('headless:false');
+    const page = await browser.newPage();
+    await page.goto(homepageLink);
 
-  await browser.close();
+    // waiting for page to load the dynamic part
+    await page.waitForFunction(() => {
+      // Check if the desired element is present or any specific condition is met
+      return document.querySelector('.h-5.hover\\:text-blue-s.dark\\:hover\\:text-dark-blue-s') !== null;
+    });
+
+    // taking page data for dom manipulation
+    const pageData = await page.content()
+    const dom = new JSDOM(pageData);
+    const document = dom.window.document;
+
+    // finding the total number of pages
+
+    const container = document.getElementsByClassName("mb-6 md:mb-0 flex flex-nowrap items-center space-x-2");
+    if (container.length==0){
+      reject(new Error('Error1: Failed to scrap the page button container!'));
+    }
+
+    const buttons = container[0].querySelectorAll("button");
+    if(buttons.length==0){
+      reject(new Error('Error2: Failed to scrap the page button container!'));
+    }
+    let numPages = 1;
+    buttons.forEach((button) => {
+      let newStr = button.textContent.trim();
+      if(newStr!=""){
+        const pgN = parseInt(newStr);
+        if(!isNaN(pgN)){
+          numPages = Math.max(numPages,pgN);
+        }
+      }
+    });
+    resolve(numPages);
+    await browser.close();
+  });
 }
 
-scrapeLeetCodeQuestions();
+getNumberOfPages()
+.then(async(result)=>{
+  const browser = await puppeteer.launch({ headless: false });
+  for(let i=1;i<=result;i++){
+    await scrapeLeetCodeQuestions(i,browser);
+  }
+  await browser.close();
+})
