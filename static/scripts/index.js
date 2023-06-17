c2e4bf06a865c5f7ab2b766c2072240b3dbbc09d
Abhishek-2k19 <abhiamisagar2512@gmail.com>
2023-06-17 15:57:43 +0530

    Added dark mode toggling feature

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,12 +1,36 @@
+// function toggleDarkMode() {
+//     var stylesheet = document.getElementById('stylesheet');
+//     console.log(stylesheet.getAttribute('href'))
+//     if (stylesheet.getAttribute('href') === "static/css/style-light.css") {
+//       stylesheet.setAttribute('href', "static/css/style-dark.css");
+//       stylesheet.classList.add('transition');
+//     } else {
+//       stylesheet.setAttribute('href', "static/css/style-light.css");
+//       stylesheet.classList.add('transition');
+//     }
+//     console.log(stylesheet.getAttribute('href'))
+//   }
+
 function toggleDarkMode() {
-    var stylesheet = document.getElementById('stylesheet');
-    console.log(stylesheet.getAttribute('href'))
-    if (stylesheet.getAttribute('href') === "static/css/style-light.css") {
-      stylesheet.setAttribute('href', "static/css/style-dark.css");
-      stylesheet.classList.add('transition');
-    } else {
-      stylesheet.setAttribute('href', "static/css/style-light.css");
-      stylesheet.classList.add('transition');
-    }
-    console.log(stylesheet.getAttribute('href'))
+  var stylesheet = document.getElementById('stylesheet');
+  var darkMode = localStorage.getItem('darkMode');
+  if (darkMode === null) {
+    darkMode = false;
+  } else {
+    darkMode = JSON.parse(darkMode);
   }
+  console.log(darkMode)
+  // Store dark mode state in local storage
+  
+  // if (stylesheet.getAttribute('href') === "{{ url_for('static', filename='css/style-light.css') }}") {
+  if (!darkMode){
+    stylesheet.setAttribute('href', "static/css/style-dark.css");
+    darkMode = true;
+    localStorage.setItem('darkMode', JSON.stringify(darkMode));
+  } else {
+    stylesheet.setAttribute('href', "static/css/style-light.css");
+    darkMode = false;
+    localStorage.setItem('darkMode', JSON.stringify(darkMode));
+  }
+  
+}
--- static/scripts/result.js
+++ static/scripts/result.js
@@ -10,3 +10,4 @@ function toggleDarkMode() {
     }
     console.log(stylesheet.getAttribute('href'))
   }
+
